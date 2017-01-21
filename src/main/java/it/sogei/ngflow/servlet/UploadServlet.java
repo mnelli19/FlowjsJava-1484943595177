af6e1961c7455a23e9b7747c6b3eb0ec60802282
Mario Nelli <mnelli@sogei.it>
2017-01-21 00:25:11 -0600

    aggiunta annotation

--- src/main/java/it/sogei/ngflow/servlet/UploadServlet.java
+++ src/main/java/it/sogei/ngflow/servlet/UploadServlet.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
